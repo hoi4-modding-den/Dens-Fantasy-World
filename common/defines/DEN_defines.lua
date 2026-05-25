148b4d5bebe9c044cb15b29e0425b23a316c8731
0rland0 <hello@om.de>
2026-05-26 01:21:40 +0200

    finished ZKN directorate tree and loading quotes

---
+++ common/defines/DEN_defines.lua
@@ -0,0 +1,38 @@
+NFactions = {
+
+    FACTION_INTELLIGENCE_ALLOWED_ADVISOR_TRAIT = {
+        "head_of_intelligence",
+		"mastermind_code_cracker",
+		"expert_code_cracker",
+		"spymaster",
+		"spymaster_no_lar",
+		"commander_of_the_fetno_derash",
+		"commander_of_the_fetno_derash_no_lar",
+		"SWI_soviet_spy",
+		"SWI_intelligence_officer",
+		"special_envoy",
+		"BRA_soviet_spy",
+		"HUN_military_intelligence_officer",
+		"AUS_secretive_priest",
+		"AUS_veteran_head_of_agency",
+		"BEL_illusive_mastermind",
+		"GER_intelligence_coordinator",
+		"GER_secretary_of_state_security",
+		"GER_reich_security_main_office_director_lar",
+		"GER_reich_security_main_office_director_no_lar",
+		"head_of_the_abwehr",
+		"head_of_the_abwehr_improved",
+		"intelligence_service_deputy",
+		"PRC_multi_talented_diplomat_lar",
+		"PRC_multi_talented_diplomat_no_lar",
+		"PRC_trained_by_the_nkvd",
+		"PRC_spymaster",
+		"PHI_intelligence_bureau_chief",
+		"HUN_stalinist_agent",
+		"JAP_tokko_chief",
+		"CHI_spymaster",
+        "ZKN_connected_mistress",
+        "ZKN_military_intelligence_officer"    
+    }
+    
+}
